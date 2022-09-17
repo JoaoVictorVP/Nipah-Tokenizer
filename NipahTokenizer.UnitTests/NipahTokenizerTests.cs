4a6f63d924cc55fd030606da731ac15b54641402
JoaoVictorVP <joaovitorsl2012@hotmail.com>
2022-09-17 15:56:53 -0300

    adding some tests to simple tokenizations

---
+++ NipahTokenizer.UnitTests/NipahTokenizerTests.cs
@@ -0,0 +1,26 @@
+using FluentAssertions;
+using System;
+using Xunit;
+namespace NipahTokenizer.UnitTests;
+
+public class NipahTokenizerTests
+{
+    [Fact]
+    public void TokenizeSimpleEntry()
+    {
+        // Arrange
+        var options = new TokenizerOptions(
+            new Separator[] { new(" ", IncludeMode.None), new("\\,"), new("\\!") }, 
+            Array.Empty<Scope>(), 
+            Array.Empty<EndOfLine>(), 
+            Array.Empty<SplitAggregator>());
+        var tokenizer = new Tokenizer();
+        string sample = "Hello, World!";
+
+        // Act
+        var tokens = tokenizer.Tokenize(sample, options);
+
+        // Assert
+        tokens.Select(x => x.text).Should().BeEquivalentTo(new[] { "Hello", ",", "World", "!" });
+    }
+}
