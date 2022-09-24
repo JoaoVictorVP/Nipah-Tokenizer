eeb99d01e1e22d8a1989b9db9069aaf32fa4c6ea
JoaoVictorVP <joaovitorsl2012@hotmail.com>
2022-09-24 16:03:34 -0300

    adding generic pool

---
+++ NipahTokenizer/GSPool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NipahTokenizer;
+
+public static class GSPool<T> where T : new()
+{
+    static readonly Queue<T> pool = new(32);
+
+    public static T Get()
+    {
+        return pool.TryDequeue(out var t) is false 
+            ? new() 
+            : t;
+    }
+    public static void Return(T instance)
+    {
+        pool.Enqueue(instance);
+    }
+}
