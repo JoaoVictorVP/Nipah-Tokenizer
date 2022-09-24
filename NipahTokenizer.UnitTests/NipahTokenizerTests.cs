d23b79e896ca1c6478ae834bb7094491fea75d0a
JoaoVictorVP <joaovitorsl2012@hotmail.com>
2022-09-24 15:04:38 -0300

    test for scopes

--- NipahTokenizer.UnitTests/NipahTokenizerTests.cs
+++ NipahTokenizer.UnitTests/NipahTokenizerTests.cs
@@ -24,4 +24,23 @@ public class NipahTokenizerTests
         // Assert
         tokens.Select(x => x.Text).Should().BeEquivalentTo(new[] { "Hello", ",", "World", "!" });
     }
+
+    [Fact]
+    public void TokenizeWithScopes()
+    {
+        // Arrange
+        var options = new TokenizerOptions(
+            new Separator[] { new(" ", IncludeMode.None), new("\\,"), new("\\!") },
+            new Scope[] { new('"', '"'), new('\'', '\'') },
+            Array.Empty<EndOfLine>(),
+            Array.Empty<SplitAggregator>());
+        var tokenizer = new Tokenizer();
+        string sample = @"This is 'a good' and highly tokenizable ""text with scopes""!";
+
+        // Act
+        var tokens = tokenizer.Tokenize(sample, options);
+
+        // Assert
+        tokens.Select(x => x.Text).Should().BeEquivalentTo(new[] { "This", "is", "'a good'", "and", "highly", "tokenizable", @"""text with scopes""", "!" });
+    }
 }
