5ec5f4e2fe5c8601022ec0bf3a29e4757a0305c0
JoaoVictorVP <joaovitorsl2012@hotmail.com>
2022-10-01 12:36:05 -0300

    .

--- NipahTokenizer.Tests/Program.cs
+++ NipahTokenizer.Tests/Program.cs
@@ -7,7 +7,7 @@ var options = new TokenizerOptions(
         Array.Empty<Scope>(),
         Array.Empty<EndOfLine>(),
         Array.Empty<SplitAggregator>(),
-        Parallel: false);
+        Parallel: true);
 string text = File.ReadAllText(Console.ReadLine());
 //string text = "Hello, World!!!";
 List<Token> tokens;
